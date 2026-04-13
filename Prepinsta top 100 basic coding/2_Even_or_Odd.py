3710ed19e695cbfa2e8748eb0681a23bfc972a68
ASUS <krishnamalgi7@gmail.com>
2026-04-13 20:00:58 +0530

    Even or Odd check

---
+++ Prepinsta top 100 basic coding/2_Even_or_Odd.py
@@ -0,0 +1,9 @@
+"""
+Program: Check if a Number is Even or Odd
+Description: Uses the modulo operator to determine divisibility by 2.
+Complexity: Time O(1) | Space O(1)
+"""
+
+num = int(input("Enter a number : "))
+
+print("Even" if num % 2 ==0 else "odd")
