267f69cbdb18c48aadc13981d24914dec1143201
ASUS <krishnamalgi7@gmail.com>
2026-04-30 18:58:33 +0530

    feat: m people can occupy r seats

---
+++ Prepinsta top 100 basic coding/34_Permutations In Which N People Can Occupy R Seats In Classroom.py
@@ -0,0 +1,51 @@
+"""
+Concept:
+Permutation means arranging r items from n distinct items where order matters.
+Formula:
+nPr = n! / (n - r)!
+
+Example:
+n = 5, r = 2 → 5P2 = 5! / 3! = 20
+
+Program: Calculate Permutations (nPr)
+Description:
+    Method 1: Using factorial calculation manually.
+    Method 2: Using built-in math.perm().
+Complexity:
+    Method 1: Time O(n) | Space O(1)
+    Method 2: Time O(r) | Space O(1)
+"""
+
+# Method 1: Using factorial (manual logic)
+def factorial(n):
+    res = 1
+    for i in range(1, n+1):
+        res *= i
+    return res
+
+n = 5
+r = 2
+
+if r > n or n < 0 or r < 0:
+    print("Invalid input")
+else:
+    result = factorial(n) // factorial(n - r)
+    print(f"Permutation is {result}")
+
+
+# Method 2: Using built-in math.perm()
+import math
+
+n = 6
+r = 3
+
+if r > n or n < 0 or r < 0:
+    print("Invalid input")
+else:
+    result = math.perm(n, r)
+    print(f"Permutation is {result}")
+
+
+# Method 1 output : Permutation is 20
+# Method 2 output : Permutation is 120
+
