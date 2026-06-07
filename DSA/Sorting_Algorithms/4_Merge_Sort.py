539924cf029e3176c174669e00ce8e923a6a6fe7
ASUS <krishnamalgi7@gmail.com>
2026-06-08 00:03:35 +0530

    feat: Added 2nd method pythonic way

--- DSA/Sorting_Algorithms/4_Merge_Sort.py
+++ DSA/Sorting_Algorithms/4_Merge_Sort.py
@@ -1,3 +1,4 @@
+#Method 1:
 def merge_arr(left, right):
     result = []
     i, j = 0, 0
@@ -46,4 +47,30 @@ sorted_nums = merge_sort(nums)
 
 print(sorted_nums)
 
+# Method 2:
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
 
+    mid = len(arr) // 2
+    left, right = merge_sort(arr[:mid]), merge_sort(arr[mid:])
+
+    result = []
+    i = j = 0
+
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+
+    # It's Pythonic shortcut instead of appending
+    result.extend(left[i:])
+    result.extend(right[j:])
+
+    return result
+
+nums = [3, 1, 2, 4, 1, 5, 2, 6, 4]
+print(merge_sort(nums))
